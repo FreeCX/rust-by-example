c24ac831f860969d9a391cfc6283ddc6231449e3
Голубев Алексей <dr.freecx@gmail.com>
2015-06-05 10:00:19 +0300

    [upd] cell count, code formating

--- brainfuck_emulator/src/main.rs
+++ brainfuck_emulator/src/main.rs
@@ -4,33 +4,38 @@ use std::fs::File;
 use std::path::Path;
 
 fn read_line( text: &str ) -> String {
-    let stdin = io::stdin();
     let mut buffer = String::new();
     print!( "{}", text );
-    io::stdout().flush().ok().expect( "Не удалось очистить stdout!");
-    stdin.lock().read_line( &mut buffer ).ok().expect( "Невозможно прочитать строку!" );
+    io::stdout().flush()
+        .ok()
+        .expect( "Не удалось очистить stdout!");
+    io::stdin().read_line( &mut buffer )
+        .ok()
+        .expect( "Невозможно прочитать строку!" );
     buffer.trim_right().to_string()
 }
 
-fn read_file( filename: String ) -> Vec<u8> {
-    let mut code: Vec<u8> = Vec::new();
+fn read_file( filename: String ) -> Vec< u8 > {
+    let mut code: Vec< u8 > = Vec::new();
     let path = Path::new( &filename );
     let mut file = match File::open( path ) {
-        Ok( file ) => file,
+        Ok( file )   => file,
         Err( error ) => panic!( "[ошибка]: {}", error )
     };
-    let result = file.read_to_end( &mut code ).unwrap();
+    file.read_to_end( &mut code )
+        .ok()
+        .expect( "Невозможно прочитать файл!" );
     code
 }
 
 fn main() {
     let filename = read_line( "Исполняемый файл: " );
-    let code: Vec<u8> = read_file( filename );
-    let mem_size: usize = 64;
+    let code: Vec< u8 > = read_file( filename );
+    let mem_size: usize = 256;
     let mut code_index: usize = 0;
     let mut cell_index: usize = 0;
-    let mut func_index: Vec<usize> = Vec::new();
-    let mut memory: Vec<i64> = ( 0 .. mem_size ).map( |_| 0 ).collect();
+    let mut func_index: Vec< usize > = Vec::new();
+    let mut memory: Vec< i64 > = ( 0 .. mem_size ).map( |_| 0 ).collect();
     while code_index < code.len() {
         match code[code_index] as char {
             '+' => memory[cell_index] += 1,
@@ -38,10 +43,7 @@ fn main() {
             '>' => cell_index += if cell_index < mem_size { 1 } else { 0 },
             '<' => cell_index -= if cell_index > 0 { 1 } else { 0 },
             '.' => print!( "{}", ( memory[cell_index] as u8 ) as char ),
-            ',' => {
-                let stdin = io::stdin();
-                memory[cell_index] = stdin.lock().bytes().next().unwrap().unwrap() as i64;
-            },
+            ',' => memory[cell_index] = io::stdin().bytes().next().unwrap().unwrap() as i64,
             '[' => func_index.push( code_index ),
             ']' => {
                 if memory[cell_index] > 0 {
