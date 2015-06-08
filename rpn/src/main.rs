ab5761341f3e2c9f5f1205a55efeea4b363b0c1d
Голубев Алексей <dr.freecx@gmail.com>
2015-06-08 13:06:54 +0300

    [add] Reverse Polish Notation example

---
+++ rpn/src/main.rs
@@ -0,0 +1,53 @@
+use std::io;
+use std::io::prelude::*;
+
+fn print_stack( stack: & Vec< f64 > ) {
+    print!( "стек < " );
+    for e in stack {
+        print!( "{} ", e );
+    }
+    print!( ">\n" );
+}
+
+fn main() {
+    let mut stack: Vec< f64 > = Vec::new();
+    print!( "введите выражение в ОПН: " );
+    io::stdout().flush()
+        .ok()
+        .expect( "[error] Can't flush to stdout!" );
+    let mut buffer = String::new();
+    io::stdin().read_line( &mut buffer )
+        .ok()
+        .expect( "[error] Can't read line from stdin!" );
+    for token in buffer.trim().split( " " ) {
+        match token {
+            "+" | "-" | "*" | "/" => {
+                let a = stack.pop().unwrap();
+                let b = stack.pop().unwrap();
+                let mut result: f64;
+                match token {
+                    "+" => result = b + a,
+                    "-" => result = b - a,
+                    "*" => result = b * a,
+                    "/" => result = b / a,
+                    _ => continue
+                };
+                println!( "{} {} {} -> {}", b, token, a, result );
+                stack.push( result );
+            },
+            _ => {
+                stack.push( match token.parse() {
+                    Ok( value ) => {
+                        println!( "{} -> stack", value );
+                        value
+                    }
+                    Err( why )  => {
+                        println!( "{}", why );
+                        continue;
+                    }
+                });
+            }
+        }
+    }
+    print_stack( &stack );
+}
