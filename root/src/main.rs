917913797c1c066365596d6641d1c2aec509740e
Голубев Алексей <dr.freecx@gmail.com>
2015-06-11 10:49:05 +0300

    [add] search root function

---
+++ root/src/main.rs
@@ -0,0 +1,17 @@
+extern crate root;
+use root::functions::*;
+
+fn f( x: f64 ) -> f64 {
+    use std::f64::consts::PI;
+    let pi4 = PI / 4.0;
+    ( x - pi4 ).cos().powi( 3 ) + ( x - pi4 ).sin().powi( 3 )
+}
+
+fn print_tuple( text: &str, tuple: ( f64, f64 ) ) {
+    let ( pi, err ) = tuple;
+    println!( "{} pi = {:.15}, err = {:.15}", text, pi, err );
+}
+
+fn main() {
+    print_tuple( "[dichotomy]", err_pi( dichotomy_1p( 3.0, 4.0, 1.0e-15_f64, f ) ) );
+}
